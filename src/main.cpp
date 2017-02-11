02f4532d349a418c3192df4650a87fcedd234af1
AdamMatus <adamkud@gmail.com>
2017-02-11 05:08:17 +0100

    test input in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,79 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
 
 
 int main()
 {
+  struct termios stdi_conf;
+  tcgetattr(STDIN_FILENO, &stdi_conf);
+  stdi_conf.c_lflag &= ~(ECHO + ICANON);
+  tcsetattr(STDIN_FILENO, TCSANOW, &stdi_conf);
+
+
+
   Paragraph_reader pr(std::string("./txts/LOTR1"));
-  int i = 10;
-  std::string un;
-  while(i--)
+
+  std::stringstream ss, ss2;
+  std::string str1;
+
+    auto par = pr.get_next_paragraph();
+    ss << par;
+    ss >> str1;
+    std::cout << par;
+
+  std::cout << str1 << std::endl << "dummy text to clear";
+  std::string str2("");
+
+  char c;
+  while(true)
   {
-    std::getline(std::cin, un);
-    std::cout << pr.get_next_paragraph();
+    ss2.clear();
+
+    c = getchar();
+    if(c == 127 || c == '\b' )
+    {
+     str2.pop_back(); 
+     ss2 << str2;
+    }
+    else
+    {
+      ss2 << str2;
+      ss2 << c;
+    }
+
+    ss2 >> str2;
+
+    std::cout << "\033[2K \r"  <<  str2;
+
+    
+
+    if(ss2.tellg() > -1 )
+    {
+      if(!str1.compare(str2)){
+        ss >>  str1;
+        std::cout << "\033[2K \r" << str1 << std::endl;
+      }
+
+      if(!str2.compare("exit"))
+      {
+        std::cout << "EXIT";
+        break;
+      }
+
+       str2 = "";
+    }
+
   }
+
+
+  stdi_conf.c_lflag |= (ECHO + ICANON);
+  tcsetattr(STDIN_FILENO, TCSANOW, &stdi_conf);
+
   return 0;
 }
